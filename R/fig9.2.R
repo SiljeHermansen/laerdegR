8b8a4f40b470f6d049b6c1439c54671252cbaa51
Silje Synnøve Lyder Hermansen <s.s.l.hermansen@stv.uio.no>
2019-03-28 17:53:11 +0100

    Update fig9.2.R

--- R/fig9.2.R
+++ R/fig9.2.R
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 fig9.2<-function(){
   #Last inn data
   data(kap9, envir = environment())
+  kap9 <- kap9
 
   #Estimer modell
   mod<-lm(Skepsis~Inntekt+
@@ -26,32 +26,3 @@ fig9.2<-function(){
          lty = 2)
 
 }
-=======
-fig9.2<-function(){
-  #Last inn data
-  data(kap9, envir = environment())
-
-  #Estimer modell
-  mod<-lm(Skepsis~Inntekt+
-                      Alder+Alder2+
-                      Prekaritet*Innvandrer,
-                    data=kap9,
-                    na.action = "na.exclude")
-  #Predikerte verdier
-  kap9$Prediksjoner<-predict(mod)
-
-  #Sett opp spredningsdiagram
-  plot(kap9$Skepsis,
-       kap9$Prediksjoner,
-       ylim = c(0,10),
-       xlab = "Observert innvandringsskepsis",
-       ylab = "Predikert innvandringsskepsis",
-       main = "Korrelasjon mellom \npredikerte og observerte y-verdier")
-
-  #Legg til hjelpelinje
-  abline(a = 0,
-         b = 1,
-         lty = 2)
-
-}
->>>>>>> 161c52947fe708930940c19d99b5bb3671c7be41
